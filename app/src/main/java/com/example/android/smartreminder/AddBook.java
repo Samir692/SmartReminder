3c7f9eebf6e961486d438326bce99e41530f7273
Samir692 <samir.huseynzade.966@gmail.com>
2018-04-11 14:29:14 +0200

    Latest code

--- app/src/main/java/com/example/android/smartreminder/AddBook.java
+++ app/src/main/java/com/example/android/smartreminder/AddBook.java
@@ -1,5 +1,6 @@
 package com.example.android.smartreminder;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,9 +12,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.android.smartreminder.database_sql.DatabaseHandler;
+
 import org.w3c.dom.Text;
 
 public class AddBook extends AppCompatActivity {
+
+    private DatabaseHandler dbHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +27,29 @@ public class AddBook extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        dbHandler = new DatabaseHandler(AddBook.this);
+
+
         final TextView bookQuestion1 = (TextView) findViewById(R.id.bookQuestion1);
         final EditText answer1 = (EditText) findViewById(R.id.answer1);
+        Button btnSetDeadline = (Button) findViewById(R.id.btnSetDeadline);
 
+
+        //get deadline from Calendar class
+        btnSetDeadline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AddBook.this, CalendarActivity.class);
+                intent.putExtra("className","com.example.android.smartreminder.AddBook");
+                startActivity(intent);
+            }
+        });
+
+        Intent incoming = getIntent();
+        final String date = incoming.getStringExtra("date");
+        System.out.println("got date = " + date);
+
+        //add Book properties
         Button addBook = (Button) findViewById(R.id.addBook);
         addBook.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,7 +62,10 @@ public class AddBook extends AppCompatActivity {
                 book.set_book_name("book_name");
                 book.set_book_total_pages(99);
                 book.set_book_done_pages(0);
-                book.set_book_deadline("11/12/2010");
+                book.set_book_deadline(date);
+
+                dbHandler.addBook(book);
+
                 //add book information depending on category
 
             }
