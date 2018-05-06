e552583d0b0d4fa13ad683ae582c000c98f4145d
Samir692 <samir.huseynzade.966@gmail.com>
2018-05-06 15:22:15 +0200

    Adding view by Java

--- app/src/main/java/com/example/android/smartreminder/HistoryActivity.java
+++ app/src/main/java/com/example/android/smartreminder/HistoryActivity.java
@@ -2,12 +2,45 @@ package com.example.android.smartreminder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.example.android.smartreminder.database_sql.DatabaseHandler;
+
+import java.util.List;
 
 public class HistoryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        DatabaseHandler dbHandler = new DatabaseHandler(HistoryActivity.this);
+
+
+
+        List<Books> allBooks = dbHandler.getAllBooks();
+
+        for (Books book : allBooks){
+            //TODO replace xml to java in order to create dynamic view
+            LinearLayout child1 = new LinearLayout(getApplicationContext());
+            child1.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            child1.setOrientation(LinearLayout.VERTICAL);
+
+            //dymanic variables
+            String date = book.get_book_deadline();
+            String bookName = book.get_book_name();
+            String bookTotalPages = Integer.toString(book.get_book_total_pages());
+            String bookDonePages = Integer.toString(book.get_book_done_pages());
+
+
+        }
+
+
+
+
+
+
+
         setContentView(R.layout.activity_history);
     }
 }
