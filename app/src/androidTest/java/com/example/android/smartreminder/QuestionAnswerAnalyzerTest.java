ad3bf3ba8666bd579e0ad93f3f49851eff133a40
Akzharkyn Duisembiyeva <aduisembiyeva@gmail.com>
2018-09-08 10:35:49 +0300

    second commit for adding analyzer using WEKA

--- app/src/androidTest/java/com/example/android/smartreminder/QuestionAnswerAnalyzerTest.java
+++ app/src/androidTest/java/com/example/android/smartreminder/QuestionAnswerAnalyzerTest.java
@@ -1,4 +1,21 @@
 package com.example.android.smartreminder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+import static org.junit.Assert.assertEquals;
 
 public class QuestionAnswerAnalyzerTest {
+    @Test
+    public void testMultipleModels() throws FOSException {
+        ModelConfig modelConfig = new ModelConfig();
+        modelConfig.setProperty(WekaModelConfig.CLASSIFIER_IMPL, Vote.class.getName());
+        modelConfig.setProperty(WekaModelConfig.CLASSIFIER_CONFIG, "-R MAX -B \""+J48.class.getName());
+
+        MultipleClassifiersCombiner classifier = (MultipleClassifiersCombiner) QuestionAnswerAnalyzer.create(modelConfig);
+
+        Assert.assertEquals(J48.class,classifier.getClassifiers()[0].getClass());
+
+    }
 }
--- app/src/main/java/com/example/android/smartreminder/QuestionAnswerAnalyzer.java
+++ app/src/main/java/com/example/android/smartreminder/QuestionAnswerAnalyzer.java
@@ -1,4 +1,10 @@
 package com.example.android.smartreminder;
+import weka.core.converters.ConverterUtils.DataSource;
+import weka.core.*;
+import weka.classifiers.Evaluation;
+import weka.classifiers.trees.J48;
 
 public class QuestionAnswerAnalyzer {
+    Classifier c = new J48();
+    trainModel(c, "J48");
 }
